06142afd7ab08e063e6f87f3ee6609392e5036f2
Jeffrey Tse <57884601+jeffery1128@users.noreply.github.com>
2022-04-28 18:04:22 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ShoppingCartComponent,
+    HomeComponent,
+    MyOrderComponent,
+    ManageOrderComponent,
+    ManageProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'shopping-cart',component:ShoppingCartComponent},
+      {path:'order',component:MyOrderComponent},
+      {path:'manage-order',component:ManageOrderComponent},
+      {path:'manage-product',component:ManageProductComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
